c1a7278cdd6c71f01cafb0119a493e010e796611
UnTalJohanPerez <kedwin.perez@ceiba.com.co>
2017-10-31 17:49:30 -0500

    udp server for nodejs

---
+++ UDP/udp.js
@@ -0,0 +1,17 @@
+var PORT = process.argv[0] || 33333;
+var HOST =  process.argv[1] ||'172.20.10.4';
+
+var dgram = require('dgram');
+var server = dgram.createSocket('udp4');
+
+server.on('listening', function () {
+    var address = server.address();
+    console.log('UDP Server listening on ' + address.address + ":" + address.port);
+});
+
+server.on('message', function (message, remote) {
+    console.log(remote.address + ':' + remote.port +' - ' + message);
+
+});
+
+server.bind(PORT, HOST);
