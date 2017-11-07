749dc5f16b498d730e24627766ca5bdeddd7b727
UnTalJohanPerez <kedwin.perez@ceiba.com.co>
2017-11-07 16:14:59 -0500

    Add udp connection

--- UDP/udp.js
+++ UDP/udp.js
@@ -1,5 +1,7 @@
-var PORT = process.argv[0] || 33333;
-var HOST =  process.argv[1] ||'172.20.10.4';
+var mySuperHub = require("./my-super-hub.js");
+
+var PORT = process.argv[2] || 33333;
+var HOST =  process.argv[3] ||'172.20.10.4';
 
 var dgram = require('dgram');
 var server = dgram.createSocket('udp4');
@@ -7,11 +9,13 @@ var server = dgram.createSocket('udp4');
 server.on('listening', function () {
     var address = server.address();
     console.log('UDP Server listening on ' + address.address + ":" + address.port);
+	mySuperHub.openConnection();
 });
 
 server.on('message', function (message, remote) {
-    console.log(remote.address + ':' + remote.port +' - ' + message);
-
+    console.log(remote.address + ':' + remote.port +' - ' + message);	
+	var data = JSON.stringify({ deviceId: 'COSensor', co: message });
+	mySuperHub.sendData(data);
 });
 
 server.bind(PORT, HOST);
